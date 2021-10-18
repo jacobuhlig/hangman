47517e4c4a841df8e1bf1fcf355473fde24f757e
jacobuhlig@gmail.com <birth_chubby_GLEN_thrips>
2021-10-18 15:44:54 +0200

    AsciiArt class created

---
+++ src/AsciiArt.java
@@ -0,0 +1,70 @@
+public class AsciiArt {
+
+    public static void progressionInHanging(int progressionInHangingValue) {
+        switch(progressionInHangingValue) {
+            case 1:
+                System.out.println("  +---+");
+                System.out.println("  |   |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("=========''', '''");
+                break;
+            case 2:
+                System.out.println("  +---+");
+                System.out.println("  |   |");
+                System.out.println("  O   |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("=========''', '''");
+                break;
+            case 3:
+                System.out.println("  +---+");
+                System.out.println("  |   |");
+                System.out.println("  O   |");
+                System.out.println("  |   |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("=========''', '''");
+                break;
+            case 4:
+                System.out.println("  +---+");
+                System.out.println("  |   |");
+                System.out.println("  O   |");
+                System.out.println(" /|   |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("=========''', '''");
+                break;
+            case 5:
+                System.out.println("  +---+");
+                System.out.println("  |   |");
+                System.out.println("  O   |");
+                System.out.println(" /|\\  |");
+                System.out.println("      |");
+                System.out.println("      |");
+                System.out.println("=========''', '''");
+                break;
+            case 6:
+                System.out.println("  +---+");
+                System.out.println("  |   |");
+                System.out.println("  O   |");
+                System.out.println(" /|\\  |");
+                System.out.println(" /    |");
+                System.out.println("      |");
+                System.out.println("=========''', '''");
+                break;
+            case 7:
+                System.out.println("  +---+");
+                System.out.println("  |   |");
+                System.out.println("  O   |");
+                System.out.println(" /|\\  |");
+                System.out.println(" / \\  |");
+                System.out.println("      |");
+                System.out.println("=========''', '''");
+                break;
+        }
+    }
+}
