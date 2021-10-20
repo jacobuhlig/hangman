85ac292d66dcbc42f897ada09902d15358df3308
jacobuhlig@gmail.com <birth_chubby_GLEN_thrips>
2021-10-20 21:10:49 +0200

    few changes to Main class hangman-wordbank.csv file added to resources repository

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 6; i++) {
+
+        File hangmanWordbank = new File(resources/hangman-wordbank.csv);
+
+
+        //Iteration of all hangman asciiArt
+        /*for (int i = 1; i <= 6; i++) {
             AsciiArt.progressionInHanging(i);
             System.out.println();
-        }
+        }*/
     }
 }
