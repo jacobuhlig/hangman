70610f9bab3483772526b9b5c5e504d865a6839e
jacobuhlig@gmail.com <birth_chubby_GLEN_thrips>
2021-10-20 22:25:11 +0200

    few changes to Main class hangman-wordbank.csv file added to resources repository

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,32 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    static ArrayList<String> easyWords = new ArrayList<String>();
+    static ArrayList<String> hardWords = new ArrayList<String>();
+
+
+    public static void fillWordbankArrays() {
+        File hangmanWordbank = new File("resources/hangman-wordbank.csv");
+        try {
+            Scanner scanWordbank = new Scanner(hangmanWordbank);
+            while(scanWordbank.hasNextLine()) {
+                String line = scanWordbank.nextLine();
+                String[] stringArray = line.split(";");
+                easyWords.add(stringArray[0]);
+                hardWords.add(stringArray[1]);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be found");
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
+        fillWordbankArrays();
 
-        File hangmanWordbank = new File(resources/hangman-wordbank.csv);
 
-
-        //Iteration of all hangman asciiArt
-        /*for (int i = 1; i <= 6; i++) {
-            AsciiArt.progressionInHanging(i);
-            System.out.println();
-        }*/
     }
 }
